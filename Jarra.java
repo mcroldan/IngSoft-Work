641f72b5d4f40c04b2dbb682caf458cd42146841
mnjimenez <nietojimenezmanuel@uma.es>
2020-03-26 11:46:24 +0100

    Programador 1

--- Jarra.java
+++ Jarra.java
@@ -3,14 +3,23 @@ private int cantidad;
 private int capacidad;
 public Jarra(int capacidadInicial){
 if(capacidadInicial<=0){
-throw new RuntimeException("ERROR: no se pueden crear jarras “ +
+throw new RuntimeException("ERROR: no se pueden crear jarras ï¿½ +
 "con capacidad menor o igual que cero.");
 }
 this.capacidad=capacidadInicial;
 }
-// Aquí vendrán después los métodos creados en la siguiente fase
+// Aquï¿½ vendrï¿½n despuï¿½s los mï¿½todos creados en la siguiente fase
 public void metodoComunitario(){
 // Comentario incluido para pruebas
 System.out.println("Aqui escribimos todos");
 }
+
+public int capacidad(){
+    return this.capacidad;
+}
+
+public int cantidad(){
+    return this.cantidad;
+}
+
 }
