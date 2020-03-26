6d2b049546b74321c18220b48b47b30e9fecdfe3
manuel-agm <manuel.agm@uma.es>
2020-03-26 12:16:09 +0100

    Ok boomer

--- Jarra.java
+++ Jarra.java
@@ -1,16 +1,26 @@
-public class Jarra {//variables de instancia
-private int cantidad;
-private int capacidad;
-public Jarra(int capacidadInicial){
-if(capacidadInicial<=0){
-throw new RuntimeException("ERROR: no se pueden crear jarras “ +
-"con capacidad menor o igual que cero.");
-}
-this.capacidad=capacidadInicial;
-}
-// Aquí vendrán después los métodos creados en la siguiente fase
-public void metodoComunitario(){
-// Comentario incluido para pruebas
-System.out.println("Aqui escribimos todos");
-}
+public class Jarra {
+	private int cantidad;
+	private int capacidad;
+	
+	public Jarra(int capacidadInicial) {
+		if(capacidadInicial<=0){
+			throw new RuntimeException("ERROR: no se pueden crear jarras â€œ +
+			"con capacidad menor o igual que cero.");
+		}
+		this.capacidad=capacidadInicial;
+	}
+	public void metodoComunitario(){
+		// Comentario incluido para pruebas
+		System.out.println("Aqui escribimos todos");
+
+	}
+	public String toString() {
+			return "La jarra contiene "+cantidad+" litros de los "+this.capacidad+" totales";
+		}
+		public void llenaDesdeJarra(Jarra j) {
+			while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
+				this.cantidad++;
+				j.cantidad--;
+			}
+		}
 }
